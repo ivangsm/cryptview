0ecd848a93b61afd39b34c0f454d27cb935d8ea6
Iván Salazar <ivangio.salazar@gmail.com>
2022-01-08 20:27:29 -0600

    feat: PubSub, Context, defimpl and defdelegate

---
+++ lib/cryptview/exchanges.ex
@@ -0,0 +1,23 @@
+defmodule Cryptview.Exchanges do
+  alias Cryptview.{Product, Trade}
+
+  @spec subscribe(Product.t()) :: :ok | {:error, term()}
+  def subscribe(product) do
+    Phoenix.PubSub.subscribe(Cryptview.PubSub, topic(product))
+  end
+
+  @spec unsubscribe(Product.t()) :: :ok | {:error, term()}
+  def unsubscribe(product) do
+    Phoenix.PubSub.unsubscribe(Cryptview.PubSub, topic(product))
+  end
+
+  @spec broadcast(Trade.t()) :: :ok | {:error, term()}
+  def broadcast(trade) do
+    Phoenix.PubSub.broadcast(Cryptview.PubSub, topic(trade.product), {:new_trade, trade})
+  end
+
+  @spec topic(Product.t()) :: String.t()
+  defp topic(product) do
+    to_string(product)
+  end
+end
--- lib/cryptview/exchanges/coinbase_client.ex
+++ lib/cryptview/exchanges/coinbase_client.ex
@@ -1,5 +1,5 @@
 defmodule Cryptview.Exchanges.CoinbaseClient do
-  alias Cryptview.{Trade, Product}
+  alias Cryptview.{Trade, Product, Exchanges}
   alias Cryptview.Exchanges.Client
   require Client
 
@@ -25,7 +25,8 @@ defmodule Cryptview.Exchanges.CoinbaseClient do
 
   @impl true
   def handle_ws_message(%{"type" => "ticker"} = msg, state) do
-    _trade = message_to_trade(msg) |> IO.inspect(label: "coinbase")
+    {:ok, trade} = message_to_trade(msg)
+    Exchanges.broadcast(trade)
     {:noreply, state}
   end
 
@@ -40,12 +41,13 @@ defmodule Cryptview.Exchanges.CoinbaseClient do
          {:ok, traded_at, _} <- DateTime.from_iso8601(msg["time"]) do
       currency_pair = msg["product_id"]
 
-      Trade.new(
-        product: Product.new(exchange_name(), currency_pair),
-        price: msg["price"],
-        volume: msg["last_size"],
-        traded_at: traded_at
-      )
+      {:ok,
+       Trade.new(
+         product: Product.new(exchange_name(), currency_pair),
+         price: msg["price"],
+         volume: msg["last_size"],
+         traded_at: traded_at
+       )}
     else
       {:error, _reason} = error -> error
     end
